5b97e13f0f11d8ea2b2ea52d2fb7e5361cf413dc
Patrick Louis <patrick@iotek.org>
2015-06-30 09:31:48 +0300

    :hammer: reverse of convert_hex_to_val.pl for the output of the network
    Former-commit-id: 47eabc68cf1326a9c1d5b1c43d0cbd639826895a

---
+++ convert_val_to_xresources_colors.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+=head1
+
+convert_val_to_xresources_colors.pl
+
+Usage ./convert_val_to_xresources_colors.pl [file with values on every line] [ratio]
+
+Use -s as input to pipe from stdin
+Converts values outputed as red, green, blue percentage to xresources colors.
+
+=cut
+
+
+sub HELP {
+	print "Usage $0 \t [file with values on every line] [ratio]\n"
+	."Converts values outputed as red, green, blue percentage to xresources colors.\n"
+	."Use -s as a file for stdin\n";
+	exit;
+}
+
+
+sub rgb_to_hex {
+	my ($red, $green, $blue, $ratio) = @_;
+	my $red_hex = sprintf("%x", ($red*255)/$ratio);
+	my $green_hex = sprintf("%x", ($green*255)/$ratio);
+	my $blue_hex = sprintf("%x", ($blue*255)/$ratio);
+	return "#$red_hex$green_hex$blue_hex";
+}
+
+
+sub convert_file {
+	my ($filename, $ratio) = @_;
+	my $fh;
+	if ($filename eq '-s') {
+		$fh = \*stdin;
+	}
+	else {
+		open($fh, "<", $filename) or die "$!";
+	}
+	while (<$fh>) {
+		chomp;
+		my ($red, $green, $blue) = $_ =~ m#(\d+\.\d+) (\d+\.\d+) (\d+\.\d+)#;
+		my $hexcode = rgb_to_hex($red, $green, $blue, $ratio);
+		print "$hexcode\n";
+	}
+}
+
+
+sub main {
+	my $filename = shift @ARGV;
+	my $ratio = shift @ARGV;
+	HELP if (!$filename || !$ratio);
+	convert_file($filename, $ratio);
+}
+
+
+main;
