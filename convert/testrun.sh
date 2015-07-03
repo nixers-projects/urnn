09a6e73822a0aa335dccc397769528894598f424
Patrick Louis <patrick@iotek.org>
2015-07-03 16:02:09 +0300

    moved place
    Former-commit-id: 0cd976fb277b44c8491b4034e85638d588beb89a

--- convert/testrun.sh
+++ convert/testrun.sh
@@ -1,1 +1,1 @@
-cat test.png | ./convert 70 | sort -k 2 -g -r | cut -d ' ' -f1 | head -n 10 | ../convert_hex_to_val.pl -s 1 | xargs -n3 | ../convert_val_to_xresources_colors.pl -s 1
+cat test.png | ./convert 70 | sort -k 2 -g -r | cut -d ' ' -f1 | head -n 10 | ../helper_scripts/convert_hex_to_val.pl -s 1 | xargs -n3 | ../helper_scripts/convert_val_to_xresources_colors.pl -s 1
