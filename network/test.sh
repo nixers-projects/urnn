206cee664baa3e5fee4a9a00890ca1589cc324b2
venam <patrick@iotek.org>
2015-07-04 14:56:44 +0300

    :hammer: test it
    Former-commit-id: 40d3fa6262cdc07f955ee10ea361c3798e31264d

--- network/test.sh
+++ network/test.sh
@@ -1,1 +1,1 @@
-cat 20.result | xargs -n3 | ../helper_scripts/convert_val_to_xresources_colors.pl -s 1 -n
+./urnn_run urnn.trained ../outputs/20.images.data | xargs -n3 | ../helper_scripts/convert_val_to_xresources_colors.pl -s 1 -n
