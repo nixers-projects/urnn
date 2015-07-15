89637a898a7073cd9dc1ad410fa0fba23e1e885a
Patrick Louis <patrick@iotek.org>
2015-07-15 09:26:44 +0300

    :hammer: perfecting the parameters to train the network
    I tested with brand new images using multiple values and those are the
    ones that gave very satisfying results.
    
    
    Former-commit-id: b5310a61859417dc9649e45659a300878f7f78f8

--- network/urnn_train.c
+++ network/urnn_train.c
@@ -31,17 +31,17 @@ main(int argc, char* argv[])
 		const unsigned int num_input = 3*10; // 3 times 10 most used colors
 		const unsigned int num_output = 3*18; // 3 times the 18 colors used in a colorscheme
 		const unsigned int num_layers = 3;
-		const unsigned int num_neurons_hidden = 5;
-		const float desired_error = (const float) 0.003; // be kinda tolerant
-		const unsigned int max_epochs = 2000000; // allow for a long run
-		const unsigned int epochs_between_reports = 30000;
+		const unsigned int num_neurons_hidden = 32;
+		const float desired_error = (const float) 0.0013; //tolerance level
+		const unsigned int max_epochs = 1500000; // allow for a long run
+		const unsigned int epochs_between_reports = 20000;
 
 		struct fann *ann = fann_create_standard(num_layers, num_input,
 				num_neurons_hidden, num_output);
 
 		//fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
 		//fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
-		fann_set_activation_steepness_output(ann, 0.2);
+		fann_set_activation_steepness_output(ann, 0.22);
 
 		fann_train_on_file(
 			ann,
