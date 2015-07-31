6abfaee382fc8f44338d45909597f1bfed32722c
Patrick Louis <patrick@iotek.org>
2015-07-31 16:18:28 +0300

    testing with 2 hidden layers
    Former-commit-id: 20a5d2d0d28a00730df34c434a2822c4d27bd763

--- network/urnn_train.c
+++ network/urnn_train.c
@@ -30,14 +30,14 @@ main(int argc, char* argv[])
 	else {
 		const unsigned int num_input = 3*10; // 3 times 10 most used colors
 		const unsigned int num_output = 3*18; // 3 times the 18 colors used in a colorscheme
-		const unsigned int num_layers = 3;
+		const unsigned int num_layers = 4;
 		const unsigned int num_neurons_hidden = 34;
 		const float desired_error = (const float) 0.0014; //tolerance level
 		const unsigned int max_epochs = 1500000; // allow for a long run
 		const unsigned int epochs_between_reports = 20000;
 
 		struct fann *ann = fann_create_standard(num_layers, num_input,
-				num_neurons_hidden, num_output);
+				num_neurons_hidden, 20, num_output);
 
 		//fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
 		//fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
