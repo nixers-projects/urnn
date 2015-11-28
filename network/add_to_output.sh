cb4e1cb8576658960bc94eb7efb2c34abb25e8bc
venam <patrick@iotek.org>
2015-11-28 09:10:09 +0200

    new dataset from shdw

--- network/add_to_output.sh
+++ network/add_to_output.sh
@@ -1,1 +1,1 @@
-../convert/sin_colors/colors -en 10 ../images/${1}.png | ../scripts/convert_hex_to_val_2.pl -s 1 > ../dataset/${1}.images.data
+../colors/sin_colors/colors -en 10 ../images/${1}.png | ../scripts/convert_hex_to_val_2.pl -s 1 > ../dataset/${1}.images.data
