7fc7ed1b991c45dbf8a55d874462ba5ace2e7a13
Nathan Isom <nathanisom27@gmail.com>
2016-04-29 02:36:50 -0500

    remove some unused reverse data, migrate some scripts, add makefile, make urnn script posix.

--- network/add_resources_to_output.sh
+++
@@ -1,1 +0,0 @@
-cat ${1}| ../scripts/extract_hex_from_xresources.pl -s | ../scripts/convert_hex_to_val_2.pl -s 1 > ../dataset/${2}.resources.data
--- network/add_to_output.sh
+++
@@ -1,1 +0,0 @@
-../colors/sin_colors/colors -en 10 ../images/${1}.png | ../scripts/convert_hex_to_val_2.pl -s 1 > ../dataset/${1}.images.data
